0d39f56900403800b2a63ec64c4dc8f7bc3f70ab
anderson.freitas <anderson.freitas@phoebus.local>
2021-09-16 09:28:09 -0300

    Controller ajustado para usar os dto's

--- src/main/java/com/dsc/student_social_network/controller/CourseController.java
+++ src/main/java/com/dsc/student_social_network/controller/CourseController.java
@@ -1,5 +1,6 @@
 package com.dsc.student_social_network.controller;
 
+import com.dsc.student_social_network.dto.CourseDto;
 import com.dsc.student_social_network.entity.Comment;
 import com.dsc.student_social_network.entity.Course;
 import com.dsc.student_social_network.services.CourseService;
@@ -19,71 +20,74 @@ public class CourseController {
     private CourseService courseService;
 
     @GetMapping("/")
-    public ResponseEntity<List<Course>> getAllCourses() {
+    public ResponseEntity<List<CourseDto>> getAllCourses() {
         return new ResponseEntity<>(courseService.getAllCourses(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> getCourseById(@PathVariable Integer id) {
+    public ResponseEntity<CourseDto> getCourseById(@PathVariable Integer id) {
         try {
             return new ResponseEntity<>(courseService.getCourseById(id), HttpStatus.OK);
-        } catch(NoSuchElementException e) {
-            return new ResponseEntity<>(new Course(null, 0), HttpStatus.NOT_FOUND);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new CourseDto(new Course(null, 0)), HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/ranking/grade")
-    public ResponseEntity<List<Course>> getRankingByGrade() {
+    public ResponseEntity<List<CourseDto>> getRankingByGrade() {
         return new ResponseEntity<>(courseService.getAllCoursesByGradeDesc(), HttpStatus.OK);
     }
 
     @PostMapping("/")
-    public ResponseEntity<Course> addCourse(@RequestBody Course newCourse) {
+    public ResponseEntity<CourseDto> addCourse(@RequestBody Course newCourse) {
         return new ResponseEntity<>(courseService.addCourse(newCourse), HttpStatus.CREATED);
     }
 
     @PutMapping("/name/{id}")
-    public ResponseEntity<Course> setNameCourseById(@PathVariable Integer id, @RequestParam(value = "name", defaultValue = "") String name) {
+    public ResponseEntity<CourseDto> setNameCourseById(@PathVariable Integer id,
+            @RequestParam(value = "name", defaultValue = "") String name) {
         try {
             return new ResponseEntity<>(courseService.setNameCourseById(id, name), HttpStatus.OK);
-        } catch(NoSuchElementException e) {
-            return new ResponseEntity<>(new Course(null, 0), HttpStatus.NOT_FOUND);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new CourseDto(new Course(null, 0)), HttpStatus.NOT_FOUND);
         }
     }
 
     @PutMapping("/grade/{id}")
-    public ResponseEntity<Course> setGradeCourseById(@PathVariable Integer id, @RequestParam(value = "grade", defaultValue = "0") String grade) {
+    public ResponseEntity<CourseDto> setGradeCourseById(@PathVariable Integer id,
+            @RequestParam(value = "grade", defaultValue = "0") Double grade) {
         try {
             return new ResponseEntity<>(courseService.setGradeCourseById(id, grade), HttpStatus.OK);
-        } catch(NoSuchElementException e) {
-            return new ResponseEntity<>(new Course(null, 0), HttpStatus.NOT_FOUND);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new CourseDto(new Course(null, 0)), HttpStatus.NOT_FOUND);
         }
     }
 
     @PutMapping("/comment/{id}")
-    public ResponseEntity<Course> addCommentToCourseById(@PathVariable Integer id, @RequestBody Comment comment) {
+    public ResponseEntity<CourseDto> addCommentToCourseById(@PathVariable Integer id, @RequestBody Comment comment) {
         try {
             return new ResponseEntity<>(courseService.addCommentToCourseById(id, comment), HttpStatus.OK);
-        } catch(NoSuchElementException e) {
-            return new ResponseEntity<>(new Course(null, 0), HttpStatus.NOT_FOUND);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new CourseDto(new Course(null, 0)), HttpStatus.NOT_FOUND);
         }
     }
 
     @DeleteMapping("/comment/{id}")
-    public ResponseEntity<Course> removeCommentToCourseById(@PathVariable Integer id, @RequestParam(value = "commentId", defaultValue = "0") String commentId) {
+    public ResponseEntity<CourseDto> removeCommentToCourseById(@PathVariable Integer id,
+            @RequestParam(value = "commentId", defaultValue = "0") String commentId) {
         try {
             return new ResponseEntity<>(courseService.removeCommentToCourseById(id, commentId), HttpStatus.OK);
-        } catch(NoSuchElementException e) {
-            return new ResponseEntity<>(new Course(null, 0), HttpStatus.NOT_FOUND);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new CourseDto(new Course(null, 0)), HttpStatus.NOT_FOUND);
         }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Course> removeCourseById(@PathVariable Integer id) {
+    public ResponseEntity<CourseDto> removeCourseById(@PathVariable Integer id) {
         try {
             return new ResponseEntity<>(courseService.removeCourseById(id), HttpStatus.OK);
-        } catch(NoSuchElementException e) {
-            return new ResponseEntity<>(new Course(null, 0), HttpStatus.NOT_FOUND);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(new CourseDto(new Course(null, 0)), HttpStatus.NOT_FOUND);
         }
     }
 }
