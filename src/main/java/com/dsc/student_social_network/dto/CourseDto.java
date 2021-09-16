6a066dbb72a5623602205bf57a815b923f90ff33
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-09-16 09:41:05 -0300

    Criação do dto da disciplina

---
+++ src/main/java/com/dsc/student_social_network/dto/CourseDto.java
@@ -0,0 +1,41 @@
+package com.dsc.student_social_network.dto;
+
+import com.dsc.student_social_network.entity.Comment;
+import com.dsc.student_social_network.entity.Course;
+import com.dsc.student_social_network.entity.Grade;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.UUID;
+
+@Data
+public class CourseDto {
+    private int id;
+    private String name;
+    private double grade;
+    private List<CommentDto> commentaries = new ArrayList<>();
+
+    public CourseDto(Course course) {
+        this.id = course.getId();
+        this.name = course.getName();
+        this.grade = getAverageGrade(course.getGradeList());
+        convertCommentaries(course.getCommentaries());
+    }
+
+    private void convertCommentaries(List<Comment> commentaries) {
+        commentaries.forEach(comment -> {
+            if (comment.getIsRemoved() == 0) {
+                var commentConverted = new CommentDto(comment);
+                this.commentaries.add(commentConverted);
+            }
+        });
+    }
+
+    private double getAverageGrade(List<Grade> grades) {
+        OptionalDouble average = grades.stream().mapToDouble(gradeObj -> gradeObj.getGrade()).average();
+        return average.isPresent() ? average.getAsDouble() : 0;
+    }
+}
