ba14bd003c0165dd86a38cc0c5fac5beac8c9323
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2021-10-20 23:07:21 -0300

    Adição do filtro JWT para endpoints especificos

--- src/main/java/com/dsc/student_social_network/StudentSocialNetworkApplication.java
+++ src/main/java/com/dsc/student_social_network/StudentSocialNetworkApplication.java
@@ -1,10 +1,23 @@
 package com.dsc.student_social_network;
 
+import com.dsc.student_social_network.filter.JWTFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudentSocialNetworkApplication {
+
+    @Bean
+    public FilterRegistrationBean<JWTFilter> filterJwt() {
+        FilterRegistrationBean<JWTFilter> filtroJwt = new FilterRegistrationBean<>();
+        filtroJwt.setFilter(new JWTFilter());
+        filtroJwt.addUrlPatterns("/courses/likes/*", "/courses/{id}/*", "/disciplinas/comment/*", "/api/login",
+                "/users/{id}");
+        return filtroJwt;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StudentSocialNetworkApplication.class, args);
